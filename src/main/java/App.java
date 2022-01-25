0d478ff7a565fbf8bfb98c929c1b857d49f0bc39
Anatoliy <honey_sunday@mail.ru>
2022-01-25 20:53:03 +0300

    Cat + tested

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,15 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld beanExtra =(HelloWorld) applicationContext.getBean("helloworld");
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean.getMessage());
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean2.getMessage());
+
+        System.out.println("==============");
+        System.out.println(bean==beanExtra);
+        System.out.println(catBean==catBean2);
+
     }
 }
--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -11,4 +11,11 @@ public class AppConfig {
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
+    @Bean(name="cat")
+    @Scope("prototype")
+    public Cat getCat(){
+        Cat cat = new Cat();
+        cat.setMessage("I'm the cat № " + Math.floor(Math.random() * 10));
+        return cat;
+    }
 }
